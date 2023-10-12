9c52640680fff67c23105bae8e92b9b963671078
linuka22 <147753570+linuka22@users.noreply.github.com>
2023-10-13 00:20:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+<html>
+    <body> 
+    <script language="javascript" type="text/javascript">
+    document.write("Hello world!");
+    // This is a comment. It is similar to comments in Java.
+    /*
+     * This is a multiline comment in JavaScript.
+     * It is very similar to comments in C programming.
+     */
+</script>
+
+    </body>
+</html>  
